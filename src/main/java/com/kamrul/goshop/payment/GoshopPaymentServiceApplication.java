71826e7eaa2f8bbdbffcdaf389eb71a65769b99c
Kamrul Bari <69103450+kamrulbari14@users.noreply.github.com>
2025-01-20 01:11:06 +0600

    chore: initiate project

---
+++ src/main/java/com/kamrul/goshop/payment/GoshopPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kamrul.goshop.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoshopPaymentServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoshopPaymentServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kamrul/goshop/payment/GoshopPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamrul.goshop.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoshopPaymentServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
